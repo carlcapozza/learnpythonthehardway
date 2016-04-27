9c5f31c143554e68020aef4f7421ca1a529b9d70
Carl <carlcapozza@gmail.com>
2016-04-27 18:37:07 -0400

    Just finishing up by adding decimals (i.e. floats).  The more you add the more specific you get

--- ex3.sd.py
+++ ex3.sd.py
@@ -2,10 +2,10 @@
 print "I will now count my chickens:"
 
 #PEDMAS 30 divided by 6 equals 5.  Add 25 and you get 30
-print "Hens", 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
 
 #PEDMAS 25 multiplied by 3 is 75.  If you divide 75 by 4 and then mulitiply the whole number result (18) by 4, you're left with 72, 75 minus 73 equals 3.  100 minus 3 equals 97. 
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # print the statement about counting your eggs
 print "Now I will count the eggs:"
@@ -16,18 +16,18 @@ print "Now I will count the eggs:"
 # 3 + 2 + 1 = 6
 # 0 + 0 + 6 = 6
 # 12 - 5 
-print 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Check if 3 + 2 (5) is less then 5 - 7 (-2). 
 # Note also that you can encapsulate equations in quotes to print them as text. 
 print "Is it true that 3 + 2 < 5 - 7?"
 
 # Return a true or false result based on the calculation
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
 # Print the questions and calculate the answer
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
 
 # Print a statement.  That's why.
 print "Oh, that's why it's False."
@@ -36,7 +36,7 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # Print some questions and evaluate based on the information provided truths provided above.
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
 
