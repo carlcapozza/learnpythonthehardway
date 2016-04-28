d934b44cfe5868f44fc268315816b14c8043bb57
Carl Capozza <carl.capozza@gianteagle.com>
2016-04-28 12:52:35 -0400

    Converted the height variable to accomodate two values, one in centimeters and one in inches

--- ex5.sd.py
+++ ex5.sd.py
@@ -2,14 +2,14 @@
 
 name = 'Zed A. Shaw' 
 age = 35 # not a lie
-height = 74 # inches 
+height_in_inches = 74 # inches 
 weight_in_lbs = 180 # lbs 
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
+print "He's %d inches tall." % height_in_inches
 print "He's %d pounds heavy." % weight_in_lbs
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
@@ -17,6 +17,6 @@ print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-   age, height, weight_in_lbs, age + height + weight_in_lb)
+   age, height_in_inches, weight_in_lbs, age + height_in_inches + weight_in_lb)
 
 
