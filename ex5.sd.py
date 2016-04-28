b78ed452abb7e1d7fc53ad08ca4c8a60c3744205
Carl Capozza <carl.capozza@gianteagle.com>
2016-04-28 13:31:10 -0400

    I have to learn more about formatters, but figured that out

--- ex5.sd.py
+++ ex5.sd.py
@@ -2,19 +2,19 @@
 
 name = 'Zed A. Shaw' 
 age = 35 # not a lie
-height_in_in = 74 # inches 
+height_in_in = 74.0 # inches 
 height_in_cm = height_in_in * 2.54 # cm
-weight_in_lbs = 180 # lbs 
-weight_in_kg = (1/2.2) * weight_in_lbs # kg
+weight_in_lbs = 180.0 # lbs 
+weight_in_kg = ( 1 / 2.2 ) * weight_in_lbs # kg
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %s." % name
 print "He's %d inches tall." % height_in_in
-print "That's %d centimeters." % height_in_cm
+print "That's %r centimeters." % height_in_cm
 print "He's %d pounds heavy." % weight_in_lbs
-print "That's %d kilograms." % weight_in_kg
+print "That's %r kilograms." % weight_in_kg
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
