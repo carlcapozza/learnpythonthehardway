185d6e176260c3a321c9ad885b6d858ac3cd4a42
Carl Capozza <carl.capozza@gianteagle.com>
2016-04-28 13:11:16 -0400

    WIP: Added two additional values for the conversions.  The conversions work but the do not include the values as float.  Reading more about that before closing this exercise.

--- ex5.sd.py
+++ ex5.sd.py
@@ -2,21 +2,25 @@
 
 name = 'Zed A. Shaw' 
 age = 35 # not a lie
-height_in_inches = 74 # inches 
+height_in_in = 74 # inches 
+height_in_cm = height_in_in * 2.54 # cm
 weight_in_lbs = 180 # lbs 
+weight_in_kg = (1/2.2) * weight_in_lbs # kg
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height_in_inches
+print "He's %d inches tall." % height_in_in
+print "That's %d centimeters." % height_in_cm
 print "He's %d pounds heavy." % weight_in_lbs
+print "That's %d kilograms." % weight_in_kg
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-   age, height_in_inches, weight_in_lbs, age + height_in_inches + weight_in_lb)
+   age, height_in_in, weight_in_lbs, age + height_in_in + weight_in_lbs)
 
 
