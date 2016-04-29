14b7b2909edeed1cf9127e40fcd1004a25b462a1
Carl Capozza <carl.capozza@gianteagle.com>
2016-04-29 11:31:47 -0400

    Study Drill using the same file.

--- ex7.py
+++ ex7.py
@@ -2,10 +2,13 @@
 
 # Print some of the different type of strings we have learned.  some with formatters.
 print "Mary had a little lamb."
+# Replace the formatter with the string 
 print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
-print "." * 10 # what'd that do?
+# print the quoted string a number of times
+print "." * 10 # what'd that do? prints out 10 . on a line.
 
+# define a string value for each variable
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -19,6 +22,8 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that comma at the end.  try removing it and see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
+# watch that comma at the end.  try removing it and see what happens  - Removing the comma makes the string print on a new line instead of inline.
+# print Cheese - if there was a comma at the end for this line it would continue the next statement on the same line.
+print end1 + end2 + end3 + end4 + end5 + end6
+# print burger 
 print end7 + end8 + end9 + end10 + end11 + end12
